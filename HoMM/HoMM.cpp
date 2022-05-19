8b3a5dc90d5dc88bfbb6b22767f639d7d5b7762c
Tobiasz2817 <Tobiasz2817@outlook.com>
2022-05-19 03:18:29 +0200

    _

--- HoMM/HoMM.cpp
+++ HoMM/HoMM.cpp
@@ -12,43 +12,46 @@
 int WINDOW_WIDTH = 15 * CELL_SIZE;
 int WINDOW_HEIGHT = 11 * CELL_SIZE;
 
-struct Vec2i
-{
-	unsigned int x;
-	unsigned int y;
-};
 struct Position
 {
-	// Start Position
-	Vec2i startPos;
+	// Start Position player
+	int startPosX;
+	int startPosY;
 
 	// Current Cell
-	Vec2i currentCell;
+	int currentCellX;
+	int currentCellY;
 
-	// Target Cell
-	Vec2i targetCell;
+	// Current Cell
+	int targetCellX;
+	int targetCellY;
 
 	// Finish Cell
-	Vec2i finishCell;
+	int finishCellX = 0;
+	int finishCellY = 0;
 };
-struct Character
+struct Player
 {
 	float speed;
 
-	Position position;
+	Position* position;
 	SDL_Texture* texture;
 
-	void Init(SDL_Texture* newTexture, int speedCharacter, unsigned int startPositioncharacterX, unsigned int startPositioncharacterY);
+	void Init(SDL_Texture* newTexture, int speedPlayer, int startPositionPlayerX, int startPositionPlayerY);
 };
+typedef struct Player Player;
 
-void Character::Init(SDL_Texture* newTexture, int speedCharacter, unsigned startPositioncharacterX, unsigned int startPositioncharacterY)
+void Player::Init(SDL_Texture* newTexture, int speedPlayer, int startPositionPlayerX, int startPositionPlayerY)
 {
 	texture = newTexture;
-	speed = speedCharacter;
+	speed = speedPlayer;
+	position->startPosX = startPositionPlayerX;
+	position->startPosY = startPositionPlayerY;
 
-	position.startPos = { startPositioncharacterX,startPositioncharacterY };
-	position.targetCell = { (position.startPos.x * CELL_SIZE) + (CELL_SIZE / 2), (position.startPos.y * CELL_SIZE) + (CELL_SIZE / 2) };
-	position.currentCell = { 0,0 };
+	position->targetCellX = (position->startPosX * CELL_SIZE) + (CELL_SIZE / 2);
+	position->targetCellY = (position->startPosY * CELL_SIZE) + (CELL_SIZE / 2);
+	position->currentCellX = 0;
+	position->currentCellY = 0;
 }
 struct Board
 {
@@ -58,58 +61,50 @@ struct Board
 	unsigned char cells[CELLS_Y][CELLS_X];
 	unsigned char cellsGrassfire[CELLS_Y][CELLS_X];
 };
-struct ListNode
-{
-	ListNode* elemnet;
-};
-struct Queue
-{
-	ListNode* firstNode;
-	void Init();
-	void AddNode();
-};
-void Queue::Init()
-{
-	firstNode = nullptr;
-}
-void Queue::AddNode()
-{
-	ListNode* new_node = (ListNode*)malloc(sizeof(ListNode));
-	new_node->elemnet = nullptr;
-
-	if (!firstNode)
-	{
-		firstNode = nullptr;
-		return;
-	}
-
-	ListNode* last_element = firstNode;
-	while (last_element->elemnet)
-		last_element = last_element->elemnet;
-
-	last_element->elemnet = new_node;
-}
-
-
+typedef struct Board Board;
 
 SDL_Texture* SetTexture(SDL_Surface* surface, SDL_Renderer* renderer, const char* fileName);
 void CreateBoard(Board* board);
 void Grassfire(Board* board);
 void DrawImage(SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect rect);
 void SetRect(SDL_Rect* rect, int x, int y, int w, int h);
-void MoveToCell(Board board, Character* character);
-bool InitSDL(SDL_Renderer** renderer, SDL_Window** window);
+void MoveToCell(Board board, Player* player);
 
 
 int main()
 {
-	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
-	if (!InitSDL(&renderer,&window))
+	// Init SDL libraries
+	SDL_SetMainReady(); // Just leave it be
+	int result = 0;
+	result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO); // Init of the main SDL library
+	if (result) // SDL_Init returns 0 (false) when everything is OK
 	{
 		printf("Can't initialize SDL. Error: %s", SDL_GetError()); // SDL_GetError() returns a string (as const char*) which explains what went wrong with the last operation
-		return 0;
+		return -1;
 	}
+
+	result = IMG_Init(IMG_INIT_PNG); // Init of the Image SDL library. We only need to support PNG for this project
+	if (!(result & IMG_INIT_PNG)) // Checking if the PNG decoder has started successfully
+	{
+		printf("Can't initialize SDL image. Error: %s", SDL_GetError());
+		return -1;
+	}
+	// Creating the window 1920x1080 (could be any other size)
+	SDL_Window* window = SDL_CreateWindow("FirstSDL",
+		0, 0,
+		WINDOW_WIDTH, WINDOW_HEIGHT,
+		SDL_WINDOW_SHOWN);
+
+	if (!window)
+		return -1;
+
+	// Creating a renderer which will draw things on the screen
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer)
+		return -1;
+
+	// Setting the color of an empty window (RGBA). You are free to adjust it.
+	SDL_SetRenderDrawColor(renderer, 128, 69, 69, 255);
 
 	SDL_Surface* surface = nullptr;
 
@@ -117,9 +112,12 @@ int main()
 	int tex_width = CELL_SIZE;
 	int tex_height = CELL_SIZE;
 
-	Character character;
+	Player player;
 
-	character.Init(SetTexture(surface, renderer, "stickXd.png"), 200.f, 0, 1);
+	Position playerPosition;
+	player.position = &playerPosition;
+
+	player.Init(SetTexture(surface, renderer, "stickXd.png"), 200.f, 4, 3);
 
 	Board board;
 	board.obstacleTexture = SetTexture(surface, renderer, "obstacle.png");
@@ -128,8 +126,8 @@ int main()
 
 	int maxDestinyReached = 2;
 
-	float x = character.position.targetCell.x;
-	float y = character.position.targetCell.y;
+	float x = player.position->targetCellX;
+	float y = player.position->targetCellY;
 
 
 
@@ -193,10 +191,10 @@ int main()
 					SDL_GetMouseState(&mousePosX, &mousePosY);
 
 					int cellX, cellY = 0;
+					player.position->finishCellX = mousePosX / tex_width;
+					player.position->finishCellY = mousePosY / tex_height;
 
-					character.position.finishCell = { (unsigned)(mousePosX / tex_width), (unsigned)(mousePosY / tex_width) };
-
-					if (board.cells[character.position.finishCell.y][character.position.finishCell.x] == 255)
+					if (board.cells[player.position->finishCellY][player.position->finishCellX] == 255)
 					{
 						canMove = false;
 						break;
@@ -205,7 +203,7 @@ int main()
 					canMove = true;
 					startGrassfire = true;
 					CreateBoard(&board);
-					board.cells[character.position.finishCell.y][character.position.finishCell.x] = 1;
+					board.cells[player.position->finishCellY][player.position->finishCellX] = 1;
 
 					break;
 				}
@@ -219,9 +217,9 @@ int main()
 
 		SDL_RenderClear(renderer);
 
-		for (int i = 0; i < CELLS_Y; i++)
+		for (int i = 0; i < CELLS_Y; ++i)
 		{
-			for (int j = 0; j < CELLS_X; j++)
+			for (int j = 0; j < CELLS_X; ++j)
 			{
 				SetRect(&rect, j * tex_width, i * tex_height, tex_width - 2, tex_height - 2);
 				DrawImage(renderer, board.cells[i][j] == 255 ? board.obstacleTexture : board.defaultTexture, rect);
@@ -229,32 +227,32 @@ int main()
 		}
 
 		SetRect(&rect, (int)round(x - tex_width / 2), (int)round(y - tex_height / 2), (int)tex_width, (int)tex_height);
-		DrawImage(renderer, character.texture, rect);
+		DrawImage(renderer, player.texture, rect);
 
 		SDL_RenderPresent(renderer);
 
-
 		if (canMove)
 		{
-			character.position.currentCell = { (unsigned)(x / CELL_SIZE), (unsigned)(y / CELL_SIZE) };
+			player.position->currentCellX = x / CELL_SIZE;
+			player.position->currentCellY = y / CELL_SIZE;
 
 			cellAreReached = true;
-			if (fabs(x - character.position.targetCell.x) >= maxDestinyReached) {
-				if (x > character.position.targetCell.x) {
-					x -= character.speed * deltaTime;
+			if (fabs(x - player.position->targetCellX) >= maxDestinyReached) {
+				if (x > player.position->targetCellX) {
+					x -= player.speed * deltaTime;
 				}
 				else {
-					x += character.speed * deltaTime;
+					x += player.speed * deltaTime;
 				}
 
 				cellAreReached = false;
 			}
-			if (fabs(y - character.position.targetCell.y) >= maxDestinyReached) {
-				if (y > character.position.targetCell.y) {
-					y -= character.speed * deltaTime;
+			if (fabs(y - player.position->targetCellY) >= maxDestinyReached) {
+				if (y > player.position->targetCellY) {
+					y -= player.speed * deltaTime;
 				}
 				else {
-					y += character.speed * deltaTime;
+					y += player.speed * deltaTime;
 				}
 				cellAreReached = false;
 			}
@@ -266,9 +264,9 @@ int main()
 				startGrassfire = false;
 			}
 
-			if (cellAreReached && (character.position.finishCell.x != character.position.currentCell.x || character.position.finishCell.y != character.position.currentCell.y))
+			if (cellAreReached && (player.position->finishCellX != player.position->currentCellX || player.position->finishCellY != player.position->currentCellY))
 			{
-				MoveToCell(board, &character);
+				MoveToCell(board, &player);
 			}
 		}
 	}
@@ -287,13 +285,13 @@ int main()
 	// Done.
 	return 0;
 }
-void MoveToCell(Board board, Character* character)
+void MoveToCell(Board board, Player* player)
 {
 	int minValue = 255;
 	int side = 0;
 
-	int currentCellX = character->position.currentCell.x;
-	int currentCellY = character->position.currentCell.y;
+	int currentCellX = player->position->currentCellX;
+	int currentCellY = player->position->currentCellY;
 
 	int newCellX = currentCellX;
 	int newCellY = currentCellY;
@@ -347,7 +345,87 @@ void MoveToCell(Board board, Character* character)
 		}
 	}
 
-	character->position.targetCell = { (unsigned)(newCellX * CELL_SIZE) + (CELL_SIZE / 2), (unsigned)(newCellY * CELL_SIZE) + (CELL_SIZE / 2) };
+	player->position->targetCellX = (newCellX * CELL_SIZE) + (CELL_SIZE / 2);
+	player->position->targetCellY = (newCellY * CELL_SIZE) + (CELL_SIZE / 2);
+
+}
+float FindCellX(Board board, Player* player)
+{
+	int minValue = 255;
+	int side = 0;
+
+	int currentCellX = player->position->currentCellX;
+	int currentCellY = player->position->currentCellY;
+
+	int newCellX = currentCellX;
+	int newCellY = currentCellY;
+
+	if (currentCellY > 0)
+	{
+		side = board.cells[(currentCellY - 1)][currentCellX];
+
+		if (side < minValue && side != 0)
+		{
+			minValue = side;
+
+			newCellX = currentCellX;
+			newCellY = (currentCellY - 1);
+		}
+	}
+	if (currentCellY < CELLS_Y - 1)
+	{
+		side = board.cells[(currentCellY + 1)][currentCellX];
+
+		if (side < minValue && side != 0)
+		{
+			minValue = side;
+
+			newCellX = currentCellX;
+			newCellY = (currentCellY + 1);
+		}
+	}
+
+	return (newCellX * CELL_SIZE) + (CELL_SIZE / 2);
+
+}
+float FindCellY(Board board, Player* player)
+{
+	int minValue = 255;
+	int side = 0;
+
+	int currentCellX = player->position->currentCellX;
+	int currentCellY = player->position->currentCellY;
+
+	int newCellX = currentCellX;
+	int newCellY = currentCellY;
+
+	if (currentCellX > 0)
+	{
+		side = board.cells[currentCellY][(currentCellX - 1)];
+
+		if (side < minValue && side != 0)
+		{
+			minValue = side;
+
+			newCellX = (currentCellX - 1);
+			newCellY = currentCellY;
+		}
+	}
+	if (currentCellX < CELLS_X - 1)
+	{
+		side = board.cells[currentCellY][(currentCellX + 1)];
+
+		if (side < minValue && side != 0)
+		{
+			minValue = side;
+
+			newCellX = (currentCellX + 1);
+			newCellY = currentCellY;
+		}
+	}
+
+	return ((newCellY * CELL_SIZE) + (CELL_SIZE / 2));
+
 }
 void Grassfire(Board* board)
 {
@@ -409,12 +487,12 @@ void Grassfire(Board* board)
 	}
 }
 
+
 SDL_Texture* SetTexture(SDL_Surface* surface, SDL_Renderer* renderer, const char* fileName)
 {
 	surface = IMG_Load(fileName);
 	if (!surface)
 	{
-
 		printf("Unable to load an image %s. Error: %s", fileName, IMG_GetError());
 		return NULL;
 	}
@@ -452,52 +530,46 @@ void CreateBoard(Board* board)
 {
 	memset(board->cells, 0, sizeof(board->cells));
 
-
+	board->cells[0][7] = 255;
 	board->cells[1][7] = 255;
-	board->cells[2][7] = 255;
-	board->cells[3][7] = 255;
-	board->cells[4][7] = 255;
-	board->cells[5][7] = 255;
-	board->cells[6][7] = 255;
+	board->cells[1][8] = 255;
+
+	board->cells[2][8] = 255;
+	board->cells[2][9] = 255;
+	board->cells[3][9] = 255;
+
+	board->cells[3][9] = 255;
+	board->cells[3][10] = 255;
+	board->cells[4][10] = 255;
+
+	board->cells[4][10] = 255;
+	board->cells[4][11] = 255;
+	board->cells[5][11] = 255;
+
+	board->cells[5][11] = 255;
+	board->cells[5][12] = 255;
+	board->cells[6][12] = 255;
+
+	board->cells[6][12] = 255;
+	board->cells[6][13] = 255;
+	board->cells[7][13] = 255;
+	board->cells[7][14] = 255;
+
+
+	board->cells[3][1] = 255;
+	board->cells[4][1] = 255;
+	board->cells[5][1] = 255;
+	board->cells[6][1] = 255;
+	board->cells[7][1] = 255;
+	board->cells[7][2] = 255;
+	board->cells[7][3] = 255;
+	board->cells[7][4] = 255;
+	board->cells[7][5] = 255;
+	board->cells[7][6] = 255;
 	board->cells[7][7] = 255;
-	board->cells[8][7] = 255;
-	board->cells[9][7] = 255;
-}
-
-bool InitSDL(SDL_Renderer** renderer, SDL_Window** window)
-{
-	// Init SDL libraries
-	SDL_SetMainReady(); // Just leave it be
-	int result = 0;
-	result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO); // Init of the main SDL library
-	if (result) // SDL_Init returns 0 (false) when everything is OK
-	{
-		printf("Can't initialize SDL. Error: %s", SDL_GetError()); // SDL_GetError() returns a string (as const char*) which explains what went wrong with the last operation
-		return false;
-	}
-
-	result = IMG_Init(IMG_INIT_PNG); // Init of the Image SDL library. We only need to support PNG for this project
-	if (!(result & IMG_INIT_PNG)) // Checking if the PNG decoder has started successfully
-	{
-		printf("Can't initialize SDL image. Error: %s", SDL_GetError());
-		return false;
-	}
-	// Creating the window 1920x1080 (could be any other size)
-	*window = SDL_CreateWindow("FirstSDL",
-		0, 0,
-		WINDOW_WIDTH, WINDOW_HEIGHT,
-		SDL_WINDOW_SHOWN);
-
-	if (!*window)
-		return false;
-
-	// Creating a renderer which will draw things on the screen
-	*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-	if (!*renderer)
-		return false;
-
-	// Setting the color of an empty window (RGBA). You are free to adjust it.
-	SDL_SetRenderDrawColor(*renderer, 128, 69, 69, 255);
-
-	return true;
+	board->cells[7][8] = 255;
+	board->cells[7][9] = 255;
+	board->cells[7][10] = 255;
+	board->cells[7][11] = 255;
+	board->cells[7][12] = 255;
 }
